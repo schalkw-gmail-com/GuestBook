8bf3a3acd7dd15cf1c7b9a083f089feec9f98e61
schalk.w <schalk@1web.co.za>
2019-05-21 18:20:48 +0200

    added messages sub menu

--- resources/views/__includes/nav_topnav.blade.php
+++ resources/views/__includes/nav_topnav.blade.php
@@ -14,9 +14,6 @@
       <li class="nav-item">
         <a class="nav-link" href="{{route('login')}}">Login</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('register')}}">Login</a>
-      </li>
     </ul>
   </div>
 </nav>
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,9 +1,14 @@
 @extends('template')
 @section('title', 'Messages')
+
+
+
 @section('content')
+  @include('__includes/nav_messages')
   <div class="container">
     <div class="jumbotron">
-      <h1>Message INdex</h1>
+      <h1>Message Index</h1>
     </div>
   </div>
 @endsection
+
